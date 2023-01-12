420285a8e09f98ecf31358b7e1e664cb108a3744
Felix Erdmann <hello@felixerdmann.com>
2023-01-12 20:55:22 +0100

    feat: redirect to first storystep

--- app/[lng]/(main)/studio/(editor)/[storyId]/page.tsx
+++ app/[lng]/(main)/studio/(editor)/[storyId]/page.tsx
@@ -1,9 +1,38 @@
 // TODO: redirect to the first story step
 
-export default async function EditorPage() {
-  return (
-    <>
-      <p>Wähle eine Slide um zu starten</p>
-    </>
-  )
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
+
+export default async function EditorPage({
+  params: { storyId },
+}: {
+  params: { storyId: string }
+}) {
+  const story = await db.story.findFirst({
+    where: {
+      id: storyId,
+    },
+    include: {
+      steps: true,
+    },
+  })
+
+  if (!story) {
+    redirect('/studio')
+  }
+
+  // redirect to first storystep
+  if (story.steps.length > 0) {
+    const { id } = story.steps[0]
+    redirect(`/studio/${storyId}/${id}`)
+  }
+
+  // create initial step if not exists
+  const initStep = await db.storyStep.create({
+    data: {
+      storyId: story.id,
+      viewport: {},
+    },
+  })
+  redirect(`/studio/${storyId}/${initStep.id}`)
 }
