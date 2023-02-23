0d1a83bcccddba0681486ef3433a1f2ea531b6d0
unknown <raabejonas@yahoo.de>
2023-02-23 14:56:33 +0100

    more descriptive names

--- src/components/Studio/Mapstories/Sidebar/MapstorySidebar.tsx
+++ src/components/Studio/Mapstories/Sidebar/MapstorySidebar.tsx
@@ -20,7 +20,7 @@ export default function MapstorySidebar({ storyID }: { storyID: string }) {
   const updateStory = useStoryStore(state => state.updateStory)
   const router = useRouter()
 
-  const markerId = useStoryStore(state => state.markerId)
+  const markerId = useStoryStore(state => state.hoverMarkerId)
   const path = usePathname()
   const stepId = path?.split('/').at(-1)
   const { story, reorderStorySteps, createStoryStep } = useStory(storyID)
