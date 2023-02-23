f4db72c876994e05fb44933775c07b4c7757e12d
PaulaScharf <paulascharf@gmx.de>
2023-02-23 12:24:19 +0100

    fix handleChange in Input

--- src/components/Elements/Input.tsx
+++ src/components/Elements/Input.tsx
@@ -32,7 +32,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputFieldProps>(
             'my-0 mb-2 block h-9 w-full rounded-md border border-slate-300 py-2 px-3 text-sm placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1',
             className,
           )}
-          onInput={e => handleChange(e.currentTarget.value)}
+          onInput={e => handleChange ? handleChange(e.currentTarget.value) : () => {}}
           ref={ref}
           size={32}
           type={type}
