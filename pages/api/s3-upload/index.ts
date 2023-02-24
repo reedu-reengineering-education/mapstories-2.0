77f416aa199beeed4b7435611906ffccb5478bfe
Thiemann96 <e_thie10@uni-muenster.de>
2023-02-24 16:27:06 +0100

    added minio to docker

--- pages/api/s3-upload/index.ts
+++ pages/api/s3-upload/index.ts
@@ -2,10 +2,10 @@ import { APIRoute } from 'next-s3-upload';
 
 
 export default APIRoute.configure({
-    accessKeyId: 'xwlBuKvxlaLZjRUu',
-    secretAccessKey: 'vSXh3wffqOm9LFABe51eobnSpqUHrHy6',
-    bucket: 'mapstories20',
+    accessKeyId: process.env.MINIO_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MINIO_SECRET_ACCESS_KEY,
+    bucket: process.env.MINIO_BUCKET,
     region: 'us-east-1',
-    endpoint: 'http://localhost:9000',
+    endpoint: process.env.MINIO_ENDPOINT,
     forcePathStyle: true
   });
