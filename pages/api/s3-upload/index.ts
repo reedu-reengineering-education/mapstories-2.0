82dd59d6aaba787f159337a186d0929fd36cb1ec
Thiemann96 <e_thie10@uni-muenster.de>
2023-02-24 18:11:23 +0100

    docker compose updated for minio

--- pages/api/s3-upload/index.ts
+++ pages/api/s3-upload/index.ts
@@ -2,8 +2,8 @@ import { APIRoute } from 'next-s3-upload';
 
 
 export default APIRoute.configure({
-    accessKeyId: process.env.MINIO_ACCESS_KEY_ID,
-    secretAccessKey: process.env.MINIO_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.MINIO_ACCESS_KEY,
+    secretAccessKey: process.env.MINIO_SECRET_KEY,
     bucket: process.env.MINIO_BUCKET,
     region: 'us-east-1',
     endpoint: process.env.MINIO_ENDPOINT,
