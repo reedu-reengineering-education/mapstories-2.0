70cf8a2346f583c8ff2581772ef276f08cb4b564
Felix Erdmann <hello@felixerdmann.com>
2023-02-28 12:58:06 +0100

    feat: validate update story request & generate new slug

--- src/lib/validations/mapstory.ts
+++ src/lib/validations/mapstory.ts
@@ -1,6 +1,14 @@
+import { Visibility } from '@prisma/client'
 import * as z from 'zod'
 
 export const createMapstorySchema = z.object({
   name: z.string().min(3).max(32),
   slug: z.string().max(32),
+})
+
+export const updateMapstorySchema = z.object({
+  name: z.string().min(3).max(32),
+  description: z.string(),
+  visibility: z.nativeEnum(Visibility),
+  theme: z.string(),
 })
