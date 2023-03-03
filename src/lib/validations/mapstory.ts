271175498785cb082285503dc858a151e8663da5
unknown <raabejonas@yahoo.de>
2023-03-03 14:01:22 +0100

    remove stuff

--- src/lib/validations/mapstory.ts
+++ src/lib/validations/mapstory.ts
@@ -1,16 +1,6 @@
 import { Visibility } from '@prisma/client'
 import * as z from 'zod'
 
-const StoryStepSchema = z.object({
-  position: z.number(),
-  storyId: z.string(),
-  feature: z.record(z.unknown()),
-  content: z.array(z.record(z.unknown())),
-  connections: z.array(z.record(z.unknown())),
-  viewport: z.record(z.unknown()),
-  storyTitleStep: z.string().optional(),
-})
-
 export const createMapstorySchema = z.object({
   name: z.string().min(3).max(32),
 })
@@ -20,5 +10,4 @@ export const updateMapstorySchema = z.object({
   description: z.string(),
   visibility: z.nativeEnum(Visibility),
   theme: z.string(),
-  firstStep: StoryStepSchema,
 })
