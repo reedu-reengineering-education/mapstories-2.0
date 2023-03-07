1ad9b9b281dd21e78966415d47b8daadec6e4941
Thiemann96 <e_thie10@uni-muenster.de>
2023-03-07 12:32:16 +0100

    include query in db log

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   if (!global.cachedPrisma) {
     global.cachedPrisma = new PrismaClient({
-      log: ['error'],
+      log: ['query', 'error'],
       errorFormat: 'pretty',
     })
   }
