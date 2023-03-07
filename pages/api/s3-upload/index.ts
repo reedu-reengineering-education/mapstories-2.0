4d48e1635f02c4484dff2197238b59d3a2c17cd6
Thiemann96 <e_thie10@uni-muenster.de>
2023-03-07 12:28:19 +0100

    use generic prefix for s3 service

--- pages/api/s3-upload/index.ts
+++ pages/api/s3-upload/index.ts
@@ -2,10 +2,10 @@ import { APIRoute } from 'next-s3-upload';
 
 
 export default APIRoute.configure({
-    accessKeyId: process.env.MINIO_ACCESS_KEY,
-    secretAccessKey: process.env.MINIO_SECRET_KEY,
-    bucket: process.env.MINIO_BUCKET,
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    bucket: process.env.S3_BUCKET,
     region: 'us-east-1',
-    endpoint: process.env.MINIO_ENDPOINT,
+    endpoint: process.env.S3_ENDPOINT,
     forcePathStyle: true
   });
