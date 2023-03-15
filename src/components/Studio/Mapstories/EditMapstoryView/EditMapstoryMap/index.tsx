546f536af61ecbfa9664d074e28cfa6bff281097
unknown <raabejonas@yahoo.de>
2023-03-15 12:24:46 +0100

    first attempt preview feature

--- src/components/Studio/Mapstories/EditMapstoryView/EditMapstoryMap/index.tsx
+++ src/components/Studio/Mapstories/EditMapstoryView/EditMapstoryMap/index.tsx
@@ -10,6 +10,9 @@ import useStory from '@/src/lib/api/story/useStory'
 import ConnectionLines from './Layers/ConnectionLines'
 import Markers from './Layers/Markers'
 import { useRouter } from 'next/navigation'
+import WebMercatorViewport from 'viewport-mercator-project'
+import { Button } from '@/src/components/Elements/Button'
+import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline'
 
 interface EditMapstoryMapProps {
   steps?: StoryStep[]
@@ -34,6 +37,12 @@ export default function EditMapstoryMap({
   const { updateStep } = useStep(storyId, currentStepId)
 
   const [markers, setMarkers] = useState<StepMarker[]>([])
+  const [viewState, setViewState] = useState({
+    longitude: 7.5,
+    latitude: 51.5,
+    zoom: 7,
+  })
+  const viewport = new WebMercatorViewport({ width: 600, height: 400 })
 
   // generate markers
   useEffect(() => {
@@ -60,6 +69,41 @@ export default function EditMapstoryMap({
     // @ts-ignore
     setMarkers(newMarkers)
   }, [currentStepId, steps])
+
+  const onPreviewClick = () => {
+    if (story?.steps) {
+      const coordinates: number[][] = []
+      story.steps.forEach(({ feature }) => {
+        const geoFeature = feature as unknown as GeoJSON.Feature<GeoJSON.Point>
+        if (geoFeature?.geometry?.coordinates?.length > 0) {
+          const longitude = geoFeature.geometry.coordinates[0]
+          const latitude = geoFeature.geometry.coordinates[1]
+          coordinates.push([latitude, longitude])
+        }
+      })
+      if (coordinates.length > 0) {
+        const bounds = coordinates.reduce(
+          (acc, curr) => {
+            return [
+              Math.min(acc[0], curr[0]), // minimum latitude
+              Math.min(acc[1], curr[1]), // minimum longitude
+              Math.max(acc[2], curr[0]), // maximum latitude
+              Math.max(acc[3], curr[1]), // maximum longitude
+            ]
+          },
+          [Infinity, Infinity, -Infinity, -Infinity],
+        )
+        const { longitude, latitude, zoom } = viewport.fitBounds(
+          [
+            [bounds[1], bounds[0]],
+            [bounds[3], bounds[2]],
+          ],
+          { padding: 20, offset: [0, -40] },
+        )
+        setViewState({ latitude, longitude, zoom })
+      }
+    }
+  }
 
   const handleMouseMove = (e: mapboxgl.MapLayerMouseEvent) => {
     const hoverSteps = e.features
@@ -99,31 +143,43 @@ export default function EditMapstoryMap({
   }
 
   return (
-    <Map
-      interactiveLayerIds={['step-hover']}
-      onClick={e => {
-        if (!steps?.find(s => s.id === currentStepId)?.feature) {
-          addMarker(e)
-        }
-      }}
-      onMouseMove={handleMouseMove}
-    >
-      <DrawControl
-        controls={{
-          polygon: true,
-          point: true,
-          trash: true,
+    <>
+      <Button
+        className="absolute right-0 top-0 z-20"
+        onClick={onPreviewClick}
+        size={'sm'}
+        startIcon={<MagnifyingGlassCircleIcon className="w-5" />}
+        variant={'inverse'}
+      >
+        Preview
+      </Button>
+      <Map
+        {...viewState}
+        interactiveLayerIds={['step-hover']}
+        onClick={e => {
+          if (!steps?.find(s => s.id === currentStepId)?.feature) {
+            addMarker(e)
+          }
         }}
-        displayControlsDefault={false}
-        position="top-left"
-      />
+        onMouseMove={handleMouseMove}
+      >
+        <DrawControl
+          controls={{
+            polygon: true,
+            point: true,
+            trash: true,
+          }}
+          displayControlsDefault={false}
+          position="top-left"
+        />
 
-      <Markers
-        markers={markers}
-        onChange={addMarker}
-        onClick={m => router.replace(`/studio/${story?.slug}/${m.stepId}`)}
-      />
-      <ConnectionLines markers={markers} />
-    </Map>
+        <Markers
+          markers={markers}
+          onChange={addMarker}
+          onClick={m => router.replace(`/studio/${story?.slug}/${m.stepId}`)}
+        />
+        <ConnectionLines markers={markers} />
+      </Map>
+    </>
   )
 }
