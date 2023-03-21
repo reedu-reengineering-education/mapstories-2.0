0a1a1267d455e5fa72c42de59c47a65136811d29
unknown <raabejonas@yahoo.de>
2023-03-21 10:12:20 +0100

    simplify

--- src/components/Studio/Mapstories/PreviewSlideButton.tsx
+++ src/components/Studio/Mapstories/PreviewSlideButton.tsx
@@ -5,19 +5,18 @@ import { useStoryStore } from '@/src/lib/store/story'
 
 export default function PreviewSlideButton() {
   const { showSlidePreview, setShowSlidePreview } = useStoryStore()
+  if (showSlidePreview) {
+    return (
+      <EyeIcon
+        className="absolute top-0 right-0 m-2 w-5 hover:cursor-pointer"
+        onClick={() => setShowSlidePreview(false)}
+      />
+    )
+  }
   return (
-    <>
-      {showSlidePreview ? (
-        <EyeIcon
-          className="absolute top-0 right-0 m-2 w-5 hover:cursor-pointer"
-          onClick={() => setShowSlidePreview(false)}
-        />
-      ) : (
-        <EyeSlashIcon
-          className="absolute top-0 right-0 m-2 w-5 hover:cursor-pointer"
-          onClick={() => setShowSlidePreview(true)}
-        />
-      )}
-    </>
+    <EyeSlashIcon
+      className="absolute top-0 right-0 m-2 w-5 hover:cursor-pointer"
+      onClick={() => setShowSlidePreview(true)}
+    />
   )
 }
