5af541aa0cfe43c787b1c667dd302c85f93698e2
Thiemann96 <e_thie10@uni-muenster.de>
2023-03-31 12:56:00 +0200

    spinner to show loading

--- src/components/Viewer/ContentTypes/MediaContent.tsx
+++ src/components/Viewer/ContentTypes/MediaContent.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import { SlideContent } from '@prisma/client'
 import Image from 'next/image'
 import { retrievePresignedUrl } from '@/src/helper/retrievePresignedUrl'
+import { Spinner } from '../../Elements/Spinner'
 
 interface MediaContentProps extends React.HTMLAttributes<HTMLFormElement> {
   content: SlideContent
@@ -10,6 +11,7 @@ interface MediaContentProps extends React.HTMLAttributes<HTMLFormElement> {
 
 export function MediaContent({ content }: MediaContentProps) {
   const [imageUrl, setImageUrl] = React.useState<any>(null)
+  const [isLoading, setIsLoading] = React.useState<boolean>(false)
   React.useEffect(() => {
     const getImageWrapper = async () => {
       await getImage(content.content)
@@ -18,15 +20,22 @@ export function MediaContent({ content }: MediaContentProps) {
   }, [])
 
   async function getImage(fileName: string) {
+    setIsLoading(true)
     const preSignedUrl = await retrievePresignedUrl('GET', fileName)
     const response = await fetch(preSignedUrl, { method: 'GET' })
     const blob = await response.blob()
     const src = URL.createObjectURL(blob)
     setImageUrl(src)
+    setIsLoading(false)
   }
 
   return (
     <div className="py-2">
+      {isLoading && (
+        <div className="flex justify-center">
+          <Spinner />
+        </div>
+      )}
       <Image
         alt={content.content}
         height={500}
