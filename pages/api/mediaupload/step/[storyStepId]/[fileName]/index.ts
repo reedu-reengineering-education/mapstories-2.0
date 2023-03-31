1f6b7c63eb63049713b8bab481c8e35069cebbc8
Thiemann96 <e_thie10@uni-muenster.de>
2023-03-31 11:08:33 +0200

    added simple prefix auth for media

--- pages/api/mediaupload/step/[storyStepId]/[fileName]/index.ts
+++ pages/api/mediaupload/step/[storyStepId]/[fileName]/index.ts
@@ -3,8 +3,13 @@ import { withMethods } from '@/src/lib/apiMiddlewares/withMethods'
 import { withAuthentication } from '@/src/lib/apiMiddlewares/withAuthentication'
 import { z } from 'zod'
 import * as minio from 'minio'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/src/lib/auth'
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
+    const session = await getServerSession(req, res, authOptions)
+    const userid = session?.user.id;
     // uploads image to minio via minio client 
     const minioClient = new minio.Client({
         endPoint: process.env.S3_ENDPOINT!,
@@ -14,10 +19,11 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
         secretKey: process.env.S3_SECRET_KEY!
     })
 
-    const fileName = req.query.fileName as string
-    const method = req.method as string
+    const fileName = `${userid}/${req.query.fileName}`;
 
-    minioClient.presignedUrl(method,'mapstories20', fileName, (err, url)=>{
+    const method = req.method as string
+    
+    minioClient.presignedUrl(method,'mapstories', fileName, (err, url)=>{
         if(err){ return console.log(err)}
         res.json(url);
       })
