74c223c68460fe1462336320a9555d93a9825d64
Thiemann96 <e_thie10@uni-muenster.de>
2023-03-31 12:23:23 +0200

    deletion and preview of pictures

--- src/components/Studio/Mapstories/SlideContentPreviewButton.tsx
+++ src/components/Studio/Mapstories/SlideContentPreviewButton.tsx
@@ -1,9 +1,11 @@
+import { retrievePresignedUrl } from '@/src/helper/retrievePresignedUrl'
 import { SlideContent } from '@prisma/client'
 
 import { cx } from 'class-variance-authority'
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
 import { OgObject } from 'open-graph-scraper/dist/lib/types'
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes, useEffect, useState } from 'react'
 import EmbedIconFactory from '../../Icons/EmbedIconFactory'
 
 const MarkdownPreview = dynamic(() => import('@uiw/react-markdown-preview'), {
@@ -31,8 +33,32 @@ export default function SlideContentPreviewButton({
 }: SlideContent) {
   const og = ogData as OgObject | null
 
+  const [imageUrl, setImageUrl] = useState(String)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    console.log(content, ogData)
+    if (type == 'IMAGE') {
+      const getImageWrapper = async () => {
+        await getImage(content)
+      }
+      getImageWrapper()
+    }
+  }, [])
+
   function IconComponent() {
     return <EmbedIconFactory className="my-1 h-8 border-none" type={type} />
+  }
+
+  async function getImage(fileName: string) {
+    setIsLoading(true)
+    const preSignedUrl = await retrievePresignedUrl('GET', fileName)
+
+    const response = await fetch(preSignedUrl, { method: 'GET' })
+    const blob = await response.blob()
+    const src = URL.createObjectURL(blob)
+    setImageUrl(src)
+    setIsLoading(false)
   }
 
   if (type == 'TEXT') {
@@ -58,6 +84,16 @@ export default function SlideContentPreviewButton({
       </Wrapper>
     )
   }
+
+  if (type == 'IMAGE') {
+    return (
+      <Wrapper>
+        <IconComponent />
+        <Image alt={content} height={50} src={imageUrl} width={50} />
+      </Wrapper>
+    )
+  }
+
   return (
     <Wrapper>
       <IconComponent />
