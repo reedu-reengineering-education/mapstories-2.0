bda6e49e8565a6faf0607e855d16812672d1e06e
Felix Erdmann <hello@felixerdmann.com>
2023-04-04 11:15:59 +0200

    chore: next/font codemod

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,5 +1,5 @@
 import '@/src/styles/globals.scss'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import { cx } from 'class-variance-authority'
 import { dir } from 'i18next'
 import dynamic from 'next/dynamic'
