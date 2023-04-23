a8159e3fd7b0189c9be484c3143f61cc6db2f8a5
Thiemann96 <e_thie10@uni-muenster.de>
2023-04-23 18:16:49 +0200

    fix

--- src/helper/getS3Image.tsx
+++ src/helper/getS3Image.tsx
@@ -1,10 +1,10 @@
 // gets the image table as parameter and queriey the s3 service for the picture and returns the url
 
-import { Image } from '@prisma/client'
+import { Media } from '@prisma/client'
 import { retrievePresignedUrl } from './retrievePresignedUrl'
 
-export async function getS3Image(image: Image) {
-  const url = await retrievePresignedUrl('GET', image.id + '.' + image.name)
+export async function getS3Image(media: Media) {
+  const url = await retrievePresignedUrl('GET', media.id + '.' + media.name)
   const response = await fetch(url, { method: 'GET' })
   const blob = await response.blob()
   const src = URL.createObjectURL(blob)
