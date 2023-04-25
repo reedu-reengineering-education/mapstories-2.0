b4a41fc17d51e8f980b77227578fb4c4230fffbb
Thiemann96 <e_thie10@uni-muenster.de>
2023-04-25 15:14:31 +0200

    added rough trranslations to 404

--- pages/404.js
+++ pages/404.js
@@ -4,12 +4,13 @@ function Custom404() {
   return (
     <div>
       <div>
-        <h1>404 - Seite nicht gefunden</h1>
+        <h1>404 - Seite nicht gefunden/Site not found</h1>
         <p>
           Die von Ihnen gesuchte Seite existiert nicht oder wurde verschoben.
         </p>
+        <p>The page you were looking for does not exist or was moved</p>
 
-        <Link href="/">Zurück zur vorherigen Seite</Link>
+        <Link href="/">Zurück zur vorherigen Seite/Back to previous page</Link>
       </div>
     </div>
   )
