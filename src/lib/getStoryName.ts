a44070564a1f537127e1b059d39c7ca4447b5c3c
Felix Erdmann <hello@felixerdmann.com>
2023-05-04 15:23:09 +0200

    chore: remove console.log

--- src/lib/getStoryName.ts
+++ src/lib/getStoryName.ts
@@ -2,7 +2,6 @@ import { Story } from '@prisma/client'
 import { db } from './db'
 
 export async function getStoryName(slug: Story['slug']) {
-  console.log('JADASJDLASJDASDAS')
   return await db.story.findFirst({
     where: {
       slug: slug,
