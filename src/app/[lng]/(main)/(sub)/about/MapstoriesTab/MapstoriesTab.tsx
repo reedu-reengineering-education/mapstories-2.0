84fdb4aeda43db7d8ceaa2a95a5e663de8442016
Thiemann96 <erictg96@googlemail.com>
2023-06-07 18:47:43 +0200

    updated partners and mapstories map

--- src/app/[lng]/(main)/(sub)/about/MapstoriesTab/MapstoriesTab.tsx
+++ src/app/[lng]/(main)/(sub)/about/MapstoriesTab/MapstoriesTab.tsx
@@ -1,8 +1,9 @@
 import { useBoundStore } from '@/src/lib/store/store'
 import { useTranslation } from '@/src/app/i18n/client'
-import Link from 'next/link'
-import { Button } from '@/src/components/Elements/Button'
-import { EnvelopeClosedIcon } from '@radix-ui/react-icons'
+import BMZLogo from '@/assets/images/partners/BMZ-Logo-4c.jpg'
+import fachStelleWeltkirche from '@/assets/images/partners/Fachstelle Weltkirche - Logo.png'
+import SUELogo from '@/assets/images/partners/SUE_Logo_CMYK_300dpi.jpg'
+import Image from 'next/image'
 
 export default function MapstoriesTab() {
   const lng = useBoundStore(state => state.language)
@@ -11,21 +12,47 @@ export default function MapstoriesTab() {
 
   return (
     <div className="flex flex-col gap-10 p-10">
-      <div className="align-center flex flex-row gap-10">
+      <div className="align-center flex flex-col gap-10">
         <div>
-          <div>
-            {/* @ts-ignore */}
-            <div>{t('text1')}</div>
+          {/* @ts-ignore */}
+          <div>{t('text1')}</div>
+          <p>{t('text2')}</p>
+          <br></br>
+          <h3>{t('sponsoredBy')}</h3>
+        </div>
+        <div className="flex flex-row justify-evenly">
+          {/* @ts-ignore */}
+          <div className="flex flex-1 flex-col items-center gap-4 border-zinc-300 p-8">
+            <div className="text-center">{t('supportBZM')}</div>
+            <a
+              className="transition duration-300 ease-in-out hover:scale-105"
+              href="https://www.bmz.de/de"
+            >
+              <Image alt="BMZ Logo" src={BMZLogo} />
+            </a>
+          </div>
+          <div className="flex flex-1 flex-col items-center gap-4 border-l-2 border-zinc-300 p-8">
+            <div>{t('supportedBy')}</div>
+            <a
+              className="transition duration-300 ease-in-out hover:scale-105"
+              href="https://www.fachstelle-weltkirche.de/"
+            >
+              <Image
+                alt="Fachstelle Weltkirche Logo"
+                src={fachStelleWeltkirche}
+              />
+            </a>
+          </div>
+          <div className="flex flex-1 flex-col items-center gap-4 border-l-2 border-zinc-300 p-8">
+            <div>{t('supportedBy')}</div>
+            <a
+              className="transition duration-300 ease-in-out hover:scale-105"
+              href="https://www.sue-umweltstiftung.org/"
+            >
+              <Image alt="SUE Logo" src={SUELogo} />
+            </a>
           </div>
         </div>
-      </div>
-      <div className="flex">
-        <Link href="mailto:mapstories@vamos-muenster.de">
-          <Button startIcon={<EnvelopeClosedIcon className="w-5" />}>
-            {/* @ts-ignore */}
-            {t('mailToButton')}
-          </Button>
-        </Link>
       </div>
     </div>
   )
