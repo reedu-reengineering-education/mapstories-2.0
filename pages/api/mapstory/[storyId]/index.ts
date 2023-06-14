c457d75621e9bd4254e414708cdb1894a9732e4b
umut <u.tas@reedu.de>
2023-06-14 10:43:37 +0200

    private public fix, hack

--- pages/api/mapstory/[storyId]/index.ts
+++ pages/api/mapstory/[storyId]/index.ts
@@ -5,12 +5,16 @@ import { db } from '@/src/lib/db'
 import { updateMapstorySchema } from '@/src/lib/validations/mapstory'
 import { generateSlug } from '@/src/lib/slug'
 import { withMapstory } from '@/src/lib/apiMiddlewares/withMapstory'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '@/src/lib/auth'
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const storyId = req.query.storyId as string
 
   if (req.method === 'GET') {
     try {
+      const session = await getServerSession(req, res, authOptions)
+      const user = session?.user
       const story = await db.story.findFirst({
         where: {
           OR: [{ id: storyId }, { slug: storyId }],
@@ -28,7 +32,12 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
           },
         },
       })
-      return res.status(200).json(story)
+
+      //AUTH HACK
+      if (story?.visibility === 'PUBLIC' || story?.ownerId === user?.id) {
+        return res.status(200).json(story)
+      }
+      return res.status(401).end()
     } catch (error) {
       return res.status(500).end()
     }
