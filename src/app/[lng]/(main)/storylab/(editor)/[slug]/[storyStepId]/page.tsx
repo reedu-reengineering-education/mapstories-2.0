2f4ce247b2ee32ec07e6faebc71e4ccbb2bfb42a
Felix Erdmann <hello@felixerdmann.com>
2023-08-11 12:18:51 +0200

    fix: show timeline just on timeline stories

--- src/app/[lng]/(main)/storylab/(editor)/[slug]/[storyStepId]/page.tsx
+++ src/app/[lng]/(main)/storylab/(editor)/[slug]/[storyStepId]/page.tsx
@@ -72,7 +72,7 @@ export default async function StepPage({
 
   return (
     <div>
-      {story && (
+      {story?.mode === 'TIMELINE' && (
         <div className="re-basic-box absolute bottom-14 left-60 z-20 min-w-[60rem] bg-white p-4">
           <EditTimelineWrapper stepId={storyStepId} storyId={story.id} />
         </div>
