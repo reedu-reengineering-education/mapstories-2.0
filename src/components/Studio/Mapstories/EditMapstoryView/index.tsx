7a06e4a4bf41a5bf2ed5ec9a26de4111779d8d8b
Thiemann96 <erictg96@googlemail.com>
2023-08-24 10:14:02 +0200

    add modal when using on small device

--- src/components/Studio/Mapstories/EditMapstoryView/index.tsx
+++ src/components/Studio/Mapstories/EditMapstoryView/index.tsx
@@ -12,6 +12,8 @@ import { fallbackLng, languages } from '@/src/app/i18n/settings'
 import { useTranslation } from '@/src/app/i18n/client'
 import { Trans } from 'react-i18next'
 import { applyTheme } from '@/src/helper/applyTheme'
+import { Modal } from '@/src/components/Modal'
+import { Button } from '@/src/components/Elements/Button'
 
 type EditMapstoryViewProps = {
   story: Story & {
@@ -25,7 +27,9 @@ export default function EditMapstoryView({ story }: EditMapstoryViewProps) {
   const [currentStep, setCurrentStep] = useState<StoryStep>()
 
   const path = usePathname()
-
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
+  const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight)
+  const [showSizeModal, setShowSizeModal] = useState<boolean>(false)
   const { story: currentStory } = useStory(story.id)
 
   let lng = useBoundStore(state => state.language)
@@ -49,6 +53,13 @@ export default function EditMapstoryView({ story }: EditMapstoryViewProps) {
     }
   }, [path, currentStory])
 
+  useEffect(() => {
+    // if window size is below 800px display a message saying that the editor is not available on mobile
+    if (windowWidth < 800 || windowHeight < 400) {
+      setShowSizeModal(true)
+    }
+  }, [windowWidth, windowHeight])
+
   if (!currentStory || !currentStep) {
     return (
       <StudioShell>
@@ -62,6 +73,23 @@ export default function EditMapstoryView({ story }: EditMapstoryViewProps) {
   return (
     // <StudioShell>
     <div className="absolute top-0 z-10 h-full w-full overflow-hidden rounded-lg shadow">
+      {showSizeModal && (
+        <Modal
+          onClose={() => setShowSizeModal(false)}
+          open={showSizeModal}
+          title={t('noMobile')}
+        >
+          <Modal.Content>
+            <p>{t('noMobileText')}</p>
+          </Modal.Content>
+          <Modal.Footer>
+            <div className="flex justify-end">
+              <Button onClick={() => setShowSizeModal(false)}>Ok</Button>
+            </div>
+          </Modal.Footer>
+        </Modal>
+      )}
+
       <div className="absolute left-1/2 top-40 z-20 w-fit -translate-x-2/4">
         <div className="mapboxgl-ctrl-group mx-auto mt-2 w-fit px-3 py-1 text-center text-sm text-black">
           {currentStep.id === story.firstStepId ? (
