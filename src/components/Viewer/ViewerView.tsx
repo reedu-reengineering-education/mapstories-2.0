af9400d12be9ae130d123833582b7da139bf61ed
Felix Erdmann <hello@felixerdmann.com>
2023-08-25 10:17:28 +0200

    fix(viewer): flyTo duration 1s

--- src/components/Viewer/ViewerView.tsx
+++ src/components/Viewer/ViewerView.tsx
@@ -343,7 +343,7 @@ export default function ViewerView({ inputStories }: ViewerViewProps) {
           offset: [-window.innerWidth / 7, -75],
           zoom: calculateZoomLogarithmic(distance),
           essential: true,
-          screenSpeed: 0.3,
+          duration: 1000,
         })
       }
     }
