6dc771e82c5531c893bdb8f2c5cac35fc474c547
umut <u.tas@reedu.de>
2023-10-10 09:56:08 +0200

    remove my stories from nav if user does not have stories

--- src/app/[lng]/(main)/(index)/layout.tsx
+++ src/app/[lng]/(main)/(index)/layout.tsx
@@ -2,9 +2,19 @@ import { UserAccountNav } from '@/src/components/Auth/UserAccountNav'
 import { Button } from '@/src/components/Elements/Button'
 import { LangSwitcher } from '@/src/components/LangSwitcher'
 import { InverseNavbar } from '@/src/components/Layout/InverseNavbar'
+import { db } from '@/src/lib/db'
 import { getCurrentUser } from '@/src/lib/session'
 import { LinkIcon } from '@heroicons/react/24/outline'
+import { User } from '@prisma/client'
 import Link from 'next/link'
+
+const countStories = async (userId: User['id']) => {
+  return await db.story.count({
+    where: {
+      ownerId: userId,
+    },
+  })
+}
 
 export default async function RootLayout({
   children,
@@ -14,12 +24,13 @@ export default async function RootLayout({
   params: { lng: string }
 }) {
   const user = await getCurrentUser()
+  const storyCount = user ? await countStories(user.id) : 0
 
   return (
     <div className="relative flex h-full flex-col">
       <header className="absolute left-0 top-0 z-10 w-full bg-opacity-50 bg-gradient-to-b from-zinc-800 to-transparent">
         <div className="container flex h-16 items-center justify-between py-4">
-          <InverseNavbar user={user}>
+          <InverseNavbar user={user} userHasStories={storyCount > 0}>
             <div className="flex space-x-3">
               <Button
                 className="mr-20 h-8 bg-zinc-700 opacity-90 hover:bg-zinc-100"
--- src/app/[lng]/(main)/(sub)/layout.tsx
+++ src/app/[lng]/(main)/(sub)/layout.tsx
@@ -5,6 +5,16 @@ import { LangSwitcher } from '@/src/components/LangSwitcher'
 import Link from 'next/link'
 import { InverseNavbar } from '@/src/components/Layout/InverseNavbar'
 import { LinkIcon } from '@heroicons/react/24/outline'
+import { User } from '@prisma/client'
+import { db } from '@/src/lib/db'
+
+const countStories = async (userId: User['id']) => {
+  return await db.story.count({
+    where: {
+      ownerId: userId,
+    },
+  })
+}
 
 export default async function RootLayout({
   children,
@@ -14,12 +24,13 @@ export default async function RootLayout({
   params: { lng: string }
 }) {
   const user = await getCurrentUser()
+  const storyCount = user ? await countStories(user.id) : 0
 
   return (
     <div className="relative flex h-full flex-col">
       <header className="absolute left-0 top-0 z-10 w-full bg-opacity-50 bg-gradient-to-b from-zinc-800 to-transparent">
         <div className="container flex h-16 items-center justify-between py-4">
-          <InverseNavbar user={user}>
+          <InverseNavbar user={user} userHasStories={storyCount > 0}>
             <div className="flex space-x-2">
               <Button
                 className="mr-20 h-8 bg-zinc-700 opacity-90 hover:bg-zinc-100"
