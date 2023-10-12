4fa26b392c2b5efc0a4ff5774bd7488642e10db0
Thiemann96 <erictg96@googlemail.com>
2023-10-12 11:48:54 +0200

    added slidelistviewer to mobile view

--- src/components/Viewer/ViewerWrapper.tsx
+++ src/components/Viewer/ViewerWrapper.tsx
@@ -18,6 +18,8 @@ import { Button } from '../Elements/Button'
 import { useTranslation } from '@/src/app/i18n/client'
 import { useBoundStore } from '@/src/lib/store/store'
 import { fallbackLng, languages } from '@/src/app/i18n/settings'
+import { CaretDownIcon } from '@radix-ui/react-icons'
+import { StorySlideListViewer } from '@/src/components/Viewer/StorySlideListViewer'
 
 type Props = {
   filter: string
@@ -32,7 +34,7 @@ export function ViewerWrapper({ filter, slug, story, tags }: Props) {
   const [showSizeModal, setShowSizeModal] = useState<boolean>(false)
   const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
   const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight)
-
+  const [openInput, setOpenInput] = useState<boolean>(false)
   let lng = useBoundStore(state => state.language)
   if (languages.indexOf(lng) < 0) {
     lng = fallbackLng
@@ -129,11 +131,28 @@ export function ViewerWrapper({ filter, slug, story, tags }: Props) {
         {slug[1] != 'start' && (
           <div className="re-basic-box z-20 h-full  max-h-full w-[55%]  self-end overflow-x-auto overflow-y-auto bg-white p-4 lg:w-[40%]">
             <div className="flex flex-row justify-evenly pt-2 lg:hidden">
+              <button
+                className="flex items-center"
+                onClick={() => setOpenInput(!openInput)}
+              >
+                <span className="whitespace-nowrap">
+                  {parseInt(slug[1]) + 1}/{story?.steps?.length}
+                </span>
+                <CaretDownIcon className="h-8 w-8"></CaretDownIcon>
+              </button>
+              <StorySlideListViewer
+                filter={'all'}
+                page={slug[1]}
+                slidesOpen={openInput}
+                slug={slug[0]}
+                story={story}
+              ></StorySlideListViewer>
               <RestartStoryButton slug={slug[0]} />
               <QuitStoryButton slug={slug[0]} />
             </div>
+
             <div
-              className="h-[320px] overflow-scroll md:h-full"
+              className="h-[320px] overflow-scroll lg:h-full"
               {...swipeHandlers}
             >
               <Slides page={slug[1]} slug={slug[0]} story={story}></Slides>
