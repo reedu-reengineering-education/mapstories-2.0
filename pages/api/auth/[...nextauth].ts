f29b9813b3c726e080182d7250844cff95dc12e9
Thiemann96 <e_thie10@uni-muenster.de>
2024-02-21 12:07:19 +0100

    fix for outlook safelinks?

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,11 @@
 import NextAuth from 'next-auth'
 import { authOptions } from '@/src/lib/auth'
-
+import type { NextApiRequest, NextApiResponse } from 'next'
 // @see ./lib/auth
+export async function auth(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'HEAD') {
+    return res.status(200).end()
+  }
+}
+
 export default NextAuth(authOptions)
