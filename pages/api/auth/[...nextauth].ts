58fcb9b3c80ea34fede7fa069b955ef816a7c3bf
Thiemann96 <e_thie10@uni-muenster.de>
2024-02-28 17:17:35 +0100

    prevent bots and preview links from invalidating the link

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,8 +7,14 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   console.log('auth api', req.method, req.url, req.query, req.body, req.headers)
   //https://next-auth.js.org/tutorials/avoid-corporate-link-checking-email-provider
   if (req.method === 'HEAD') {
-    console.log('HEAD request');
-    return res.status(200).end()
+    res.status(200).end(); 
+    return;
+  }
+  // if user agent is a bot, return 404
+  
+  if (req.headers['user-agent']?.match(/bot|preview|crawler|spider/i)) {
+    res.status(200).end()
+    return
   }
 
   // Do whatever you want here, before the request is passed down to `NextAuth`
