cf8df1b64125af84d2d09e33762b9b22dac9d199
Thiemann96 <e_thie10@uni-muenster.de>
2024-02-28 16:37:56 +0100

    prevent head reqeusts from expiring the link

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,14 @@
 import NextAuth from 'next-auth'
-import { authOptions } from '@/src/lib/auth'
 import type { NextApiRequest, NextApiResponse } from 'next'
-// @see ./lib/auth
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'HEAD') {
-    console.log('hallo')
-    res.status(200).end()
-  } 
-  return NextAuth(authOptions)
-}
+import { authOptions } from '@/src/lib/auth'
 
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  //https://next-auth.js.org/tutorials/avoid-corporate-link-checking-email-provider
+  if (req.method === 'HEAD') {
+    return res.status(200).end()
+  }
+
+  // Do whatever you want here, before the request is passed down to `NextAuth`
+  return await NextAuth(req, res, authOptions)
+}
