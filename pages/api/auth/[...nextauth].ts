084af27eb3707cfe2a2760ebe77dbc4dda1a1ed5
Thiemann96 <e_thie10@uni-muenster.de>
2024-02-28 16:31:17 +0100

    retrigger build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'HEAD') {
     console.log('hallo')
     res.status(200).end()
-  }
+  } 
   return NextAuth(authOptions)
 }
 
