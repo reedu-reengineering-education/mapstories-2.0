8b4bd3d8f7f0730dcc72a7ff02e9ec2574751181
Thiemann96 <e_thie10@uni-muenster.de>
2024-03-01 10:07:57 +0100

    removed logs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -30,7 +30,6 @@ export const authOptions: NextAuthOptions = {
             pass: process.env.SMTP_PASS,
           },
         })
-        console.log('new log in mail has been sent: ', url)
         const emailHtml = render(SignInEmail({ url }))
 
         const options: MailOptions = {
