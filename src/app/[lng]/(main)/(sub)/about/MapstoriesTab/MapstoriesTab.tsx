b8918c5174537546ddc5710143f4dd8dabca09a3
Thiemann96 <erictg96@gmail.com>
2024-03-20 13:11:47 +0100

    edited partner logos

--- src/app/[lng]/(main)/(sub)/about/MapstoriesTab/MapstoriesTab.tsx
+++ src/app/[lng]/(main)/(sub)/about/MapstoriesTab/MapstoriesTab.tsx
@@ -60,7 +60,7 @@ export default function MapstoriesTab() {
               {t('supportedBy')}
             </div>
             <a
-              className="transition duration-300 ease-in-out hover:scale-105"
+              className="transition duration-300 ease-in-out hover:scale-105 flex justify-center align-baseline"
               href="https://www.bistum-muenster.de/weltkirche"
             >
               <Image
@@ -73,7 +73,7 @@ export default function MapstoriesTab() {
             {/* @ts-ignore */}
             <div className="hidden text-center lg:block">{t('supportBZM')}</div>
             <a
-              className="transition duration-300 ease-in-out hover:scale-105"
+              className="transition duration-300 ease-in-out hover:scale-105 flex justify-center align-baseline"
               href="https://www.bmz.de/de"
             >
               <Image alt="BMZ Logo" src={BMZLogo} />
@@ -85,7 +85,7 @@ export default function MapstoriesTab() {
               {t('supportedBy')}
             </div>
             <a
-              className="transition duration-300 ease-in-out hover:scale-105"
+              className="transition duration-300 ease-in-out hover:scale-105 flex justify-center align-baseline"
               href="https://www.sue-nrw.de/"
             >
               <Image alt="SUE Logo" src={SUELogo} />
@@ -97,24 +97,24 @@ export default function MapstoriesTab() {
             Gefördert durch das Ministerium für Kultur und Wissenschaft des Landes Nordrhein-Westfalen und die Kunststiftung NRW im Rahmen des Projektes ›zeitgeisty. be part of … Protestkulturen 1957-1967‹, getragen vom Bunker Ulmenwall e.V.
             </div>
             <div className='flex flex-col lg:flex-row align-baseline'>
-            <div className="flex flex-1 flex-col  items-center gap-4 p-8 lg:border-r-2 my-2 lg:border-zinc-300">
-            {/* @ts-ignore */}
-            <a
-              className="transition duration-300 ease-in-out hover:scale-105"
-              href="https://www.kunststiftungnrw.de/"
-            >
-              <Image alt="Kunststiftung NRW" src={kunstNRW} />
-            </a>
-          </div>
-          <div className="flex flex-1 flex-col  items-center gap-4 p-8">
-            {/* @ts-ignore */}
-            <a
-              className="transition duration-300 ease-in-out hover:scale-105"
-              href="https://www.mkw.nrw"
-            >
-              <Image alt="Kunst & Kultur NRW" src={kulturWissenschaftNRW} />
-            </a>
-          </div>
+              <div className="flex flex-1 flex-col  items-center gap-4 p-8">
+              {/* @ts-ignore */}
+              <a
+                className="transition duration-300 ease-in-out hover:scale-105 flex justify-center align-baseline"
+                href="https://www.mkw.nrw"
+              >
+                <Image alt="Kunst & Kultur NRW" className="kulturWissenschaft" src={kulturWissenschaftNRW} />
+              </a>
+            </div>
+              <div className="flex flex-1 flex-col  items-center gap-4 p-8 lg:border-l-2 my-2 lg:border-zinc-300">
+              {/* @ts-ignore */}
+              <a
+                className="transition duration-300 ease-in-out hover:scale-105 flex justify-center  align-baseline"
+                href="https://www.kunststiftungnrw.de/"
+              >
+                <Image alt="Kunststiftung NRW" src={kunstNRW} />
+              </a>
+            </div>
             </div>
         </div>
         </CardContent>
