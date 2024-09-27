cb2b934271ddbee63951596ef9debac771ba3720
Eric Thieme-Garmann <erictg96@gmail.com>
2024-09-27 11:35:25 +0200

    new markdown in every text component

--- src/components/Viewer/CommunityStep/ContentEdits/TextContentEdit.tsx
+++ src/components/Viewer/CommunityStep/ContentEdits/TextContentEdit.tsx
@@ -7,6 +7,18 @@ import { useState } from 'react'
 import '@uiw/react-md-editor/markdown-editor.css'
 import '@uiw/react-markdown-preview/markdown.css'
 import dynamic from 'next/dynamic'
+import {
+  Bold,
+  Code,
+  HeadingIcon,
+  HelpCircleIcon,
+  Italic,
+  Link,
+  List,
+  Table,
+} from 'lucide-react'
+import { commands } from '@uiw/react-md-editor'
+import { ToolbarButton } from '@/src/components/Elements/MarkdownToolbar'
 
 interface TextContentEditProps extends React.HTMLAttributes<HTMLFormElement> {
   stepItem: any
@@ -56,9 +68,91 @@ export function TextContentEdit({
         <div>
           {/* @ts-ignore */}
           <MDEditor
+            commands={[
+              commands.bold,
+              commands.italic,
+              commands.title,
+              commands.link,
+              commands.code,
+              commands.table,
+              commands.orderedListCommand,
+              commands.help,
+            ]}
+            components={{
+              toolbar: (command, disabled, executeCommand) => {
+                switch (command.name) {
+                  case 'bold':
+                    return (
+                      <ToolbarButton
+                        icon={<Bold className="h-4 w-4" />}
+                        label="Fett"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  case 'italic':
+                    return (
+                      <ToolbarButton
+                        icon={<Italic className="h-4 w-4" />}
+                        label="Kursiv"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  case 'title1':
+                    return (
+                      <ToolbarButton
+                        icon={<HeadingIcon className="h-4 w-4" />}
+                        label="Überschrift"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  case 'code':
+                    return (
+                      <ToolbarButton
+                        icon={<Code className="h-4 w-4" />}
+                        label="Code"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  case 'link':
+                    return (
+                      <ToolbarButton
+                        icon={<Link className="h-4 w-4" />}
+                        label="Link"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  case 'table':
+                    return (
+                      <ToolbarButton
+                        icon={<Table className="h-4 w-4" />}
+                        label="Tabelle"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  case 'ordered-list':
+                    return (
+                      <ToolbarButton
+                        icon={<List className="h-4 w-4" />}
+                        label="Liste"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  case 'help':
+                    return (
+                      <ToolbarButton
+                        icon={<HelpCircleIcon className="h-4 w-4" />}
+                        label="Liste"
+                        onClick={() => executeCommand(command)}
+                      />
+                    )
+                  default:
+                    return
+                }
+              },
+            }}
             data-color-mode="light"
+            extraCommands={[]}
             onChange={setTextValue}
-            preview="edit"
             value={textValue}
           />
         </div>
