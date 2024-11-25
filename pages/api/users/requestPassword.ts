0ffe8446f29bf877fcff30fb373bd376b99cef1e
Eric Thieme-Garmann <erictg96@gmail.com>
2024-11-25 14:52:46 +0100

    able to request a new password which is send via mail

---
+++ pages/api/users/requestPassword.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/src/lib/db'
+import nodemailer from 'nodemailer'
+import crypto from 'crypto'
+import { withMethods } from '@/src/lib/apiMiddlewares/withMethods'
+import bcrypt from 'bcrypt'
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { email } = req.body
+
+  if (!email) {
+    return res.status(400).json({ message: 'Email is required' })
+  }
+
+  try {
+    // Generate a new random password
+    const newPassword = crypto.randomBytes(8).toString('hex')
+    const hashedPassword = await bcrypt.hash(newPassword, 10)
+
+    // Update the user's password in the database
+    const updatedUser = await db.user.findUnique({
+      where: { email },
+    })
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    if (updatedUser.password && updatedUser.password.length > 0) {
+      return res.status(400).json({
+        message: 'User already has a password set',
+      })
+    }
+    await db.user.update({
+      where: { email },
+      data: { password: hashedPassword },
+    })
+
+    // Send an email to the user with the new password
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    })
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Your new password',
+      text: `Your new password is: ${newPassword}`,
+    }
+    await transporter.sendMail(mailOptions)
+
+    res
+      .status(200)
+      .json({ message: 'New password has been set and emailed to you' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
+
+export default withMethods(['POST'], handler)
---
+++ src/app/[lng]/(auth)/requestPassword/page.tsx
@@ -0,0 +1,38 @@
+import { useTranslation } from '@/src/app/i18n'
+import UserAuthRequestPassword from '@/src/components/Auth/UserAuthRequestPassword'
+import { LogoWithClaimAndBackground } from '@/src/components/Layout/MapstoriesLogo'
+import React from 'react'
+
+export default async function RequestPasswordPage({
+  params: { lng },
+}: {
+  params: { lng: string }
+}) {
+  const { t } = await useTranslation(lng, 'login')
+
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <LogoWithClaimAndBackground className="mx-auto h-32 w-60" />
+          <h1 className="text-2xl font-bold">{t('welcome_back')}</h1>
+          <p className="text-sm text-slate-600">
+            Enter email below to request a password for your account
+          </p>
+        </div>
+        <UserAuthRequestPassword />
+        {/* <h1 className="text-center">Noch keinen Account?</h1>
+        <p className="px-8 text-center text-sm text-slate-600">
+          {t('disclaimerRegister')}{' '}
+          <Link className="hover:text-brand underline" href="/terms">
+            {t('TOS')}
+          </Link>{' '}
+          {t('and')}{' '}
+          <Link className="hover:text-brand underline" href="/privacy">
+            {t('PP')}
+          </Link>
+        </p> */}
+      </div>
+    </div>
+  )
+}
