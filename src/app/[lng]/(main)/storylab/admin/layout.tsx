4d654802b1d3a4cde1a11b87a6b410055b238d67
Thiemann96 <e.thieme@reedu.de>
2026-09-03 08:48:56 +0200

    remove roadmap / feedback button

--- src/app/[lng]/(main)/storylab/admin/layout.tsx
+++ src/app/[lng]/(main)/storylab/admin/layout.tsx
@@ -1,14 +1,11 @@
 import { UserAccountNav } from '@/src/components/Auth/UserAccountNav'
-import { Button } from '@/src/components/Elements/Button'
 import { LangSwitcher } from '@/src/components/LangSwitcher'
 import { Footer } from '@/src/components/Layout/Footer'
 import { Navbar } from '@/src/components/Layout/Navbar'
 import { StudioSidebar } from '@/src/components/Studio/Sidebar'
 import { db } from '@/src/lib/db'
 import { getCurrentUser } from '@/src/lib/session'
-import { LinkIcon } from '@heroicons/react/24/outline'
 import { User } from '@prisma/client'
-import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 const countStories = async (userId: User['id']) => {
@@ -45,22 +42,11 @@ export default async function AdminLayout({
         <div className="flex h-16 items-center justify-between border-b border-b-slate-200 py-4">
           <Navbar user={user} userHasStories={storyCount > 0}>
             <div className="flex space-x-2">
-              <Button
-                className="mr-20 h-8 bg-zinc-700 opacity-90 hover:bg-zinc-100"
-                startIcon={<LinkIcon className="w-5" />}
-              >
-                {' '}
-                <a
-                  href="https://www.taskcards.de/#/board/1b41a521-922e-471c-949b-b0d132c903c7/view "
-                  target="_blank"
-                >
-                  Roadmap
-                </a>
-              </Button>
               <LangSwitcher />
+          <UserAccountNav user={user} />
+
             </div>
           </Navbar>
-          <UserAccountNav user={user} />
         </div>
       </header>
       <div className="container flex h-full flex-1 gap-12 py-8">
